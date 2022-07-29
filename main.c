1b2550632446e7022e871e59483549963ecb0445
Joshthefullstack <104472181+Joshthefullstack@users.noreply.github.com>
2022-07-29 07:54:23 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - psimple shell program to print hello world.
+ * argc: number of argument
+ * argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *[argv])
+{
+	printf("hello world\n");
+	return (0);
+}
